1eea99f62255aa5c738679d3cdfc3a7f8c643e9f
vyombhardwaj <166542158+vyombhardwaj@users.noreply.github.com>
2026-07-22 19:28:02 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import "./globals.css";
 import { Providers } from "./providers";
+import type { Metadata, Viewport } from "next";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
